dbd13acbed21984ca30d78de8bc501ee5d6c216f
sulla <inikolazzz@gmail.com>
2020-12-04 11:37:50 +0300

    end of lesson 4

--- alg_20112020/src/lesson3/MyDeque.java
+++ alg_20112020/src/lesson3/MyDeque.java
@@ -1,114 +1,107 @@
 package lesson3;
 
-
-import java.util.NoSuchElementException;
+import java.util.Arrays;
+import java.util.EmptyStackException;
 
 public class MyDeque<T> {
-
-    private T[] list;
-    private int size;
-    private final int DEFAULT_CAPACITY = 10;
-    private int begin;
-    private int end;
-
-    public MyDeque(int capacity) {
-        if (capacity <= 0) {
-            throw new IllegalArgumentException("capacity: " + capacity);
-        }
-        list = (T[]) new Object[capacity];
-    }
+    private T[] arr;
+    private int begin = 0;
+    private int end = 0;
+    private int size = 0;
+    private double loadFactor = 0.75;
 
     public MyDeque() {
-        list = (T[]) new Object[DEFAULT_CAPACITY];
+        this(10);
     }
 
+    public MyDeque(int num) {
+        this(num, 0.75);
+    }
 
-    void insertFront(T item) {
-        if (isFull()) {
-            throw new IllegalStateException();
-        }
-        if (begin == -1) {
-            begin = 0;
-            end = 0;
-        } else if (begin == 0) {
-            begin = size - 1;
+    public MyDeque(int num, double loadFactor) {
+        setLoadFactor(loadFactor);
+        if (num > 0) {
+            arr = (T[]) new Object[num];
         } else {
-            begin = begin - 1;
+            throw new IllegalArgumentException();
         }
-        list[begin] = item;
     }
 
-    void insertRear(T item) {
-        if (isFull()) {
-            throw new IllegalStateException();
-        }
-        if (end == -1) {
-            begin = 0;
-            end = 0;
-        } else if (end == size - 1) {
-            end = 0;
-        } else {
-            end = end + 1;
-        }
-        list[end] = item;
+    public void addRight(T value) {
+        size++;
+        expand();
+        arr[end] = value;
+        end = nextIndex(end);
     }
 
-
-    public T peekFront() {
-        if (isEmpty()) {
-            throw new NoSuchElementException();
-        }
-        return list[begin];
+    public void addLeft(T value) {
+        size++;
+        expand();
+        begin = lastIndex(begin);
+        arr[begin] = value;
     }
 
-
-    public T peekRear() {
-        if (isEmpty()) {
-            throw new NoSuchElementException();
+    public T peekRight() {
+        if (size <= 0) {
+            throw new EmptyStackException();
         }
-        return list[end];
+        return arr[begin];
     }
 
-    void removeFront() {
-        if (isEmpty()) {
-            throw new NoSuchElementException();
+    public T peekLeft() {
+        if (size <= 0) {
+            throw new EmptyStackException();
         }
-        if (begin == end) {
-            begin = -1;
-            end = -1;
-        } else {
-            if (begin == size - 1) {
-                begin = 0;
+        return arr[lastIndex(end)];
+    }
+
+    public T removeRight() {
+        T value = peekRight();
+        size--;
+        arr[begin] = null;
+        begin = nextIndex(begin);
+        return value;
+    }
+
+    public T removeLeft() {
+        T value = peekLeft();
+        size--;
+        end = lastIndex(end);
+        arr[end] = null;
+        return value;
+    }
+
+    private int nextIndex(int index) {
+        return (index + 1) % arr.length;
+    }
+
+    private int lastIndex(int index) {
+        return (arr.length + index - 1) % arr.length;
+    }
+
+    public void info() {
+        System.out.print(Arrays.toString(arr));
+        System.out.printf("begin: %d end: %d size %d \n", begin, end, size);
+    }
+
+    private final void setLoadFactor(double loadFactor) {
+        if (loadFactor > 0 && loadFactor < 1) {
+            this.loadFactor = loadFactor;
+        }
+    }
+
+    private void expand() {
+        if ((double) size / arr.length > loadFactor) {
+            T[] tempArr = (T[]) new Object[arr.length * 2];
+            if (begin <= end) {
+                System.arraycopy(arr, 0, tempArr, 0, arr.length);
             } else {
-                begin = begin + 1;
+                System.arraycopy(arr, 0, tempArr, 0, end);
+                int newBegin = tempArr.length - arr.length + begin;
+                System.arraycopy(arr, begin, tempArr, newBegin, arr.length - begin);
+                begin = newBegin;
             }
+            arr = tempArr;
         }
     }
-
-
-    void removeRear() {
-        if (isEmpty()) {
-            throw new NoSuchElementException();
-        }
-        if (begin == end) {
-            begin = -1;
-            end = -1;
-        } else if (end == 0) {
-            end = size - 1;
-        } else {
-            end = end - 1;
-        }
-
-    }
-
-
-    boolean isFull() {
-        return ((begin == 0 && end == size - 1) || begin == end + 1);
-    }
-
-    boolean isEmpty() {
-        return (begin == -1);
-    }
-
-
 }
